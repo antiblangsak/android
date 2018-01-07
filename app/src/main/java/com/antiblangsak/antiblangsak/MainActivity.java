ceb75999f249525500161c809f94a69acdcff925
Dhanang <dhananghadhi@gmail.com>
2018-01-07 23:27:41 +0700

    profile activity (susulan)

--- app/src/main/java/com/antiblangsak/antiblangsak/MainActivity.java
+++ app/src/main/java/com/antiblangsak/antiblangsak/MainActivity.java
@@ -1,11 +1,13 @@
 package com.antiblangsak.antiblangsak;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
